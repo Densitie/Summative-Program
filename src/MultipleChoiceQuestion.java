1faa9e43e0376414fef337e092dbc2c1f1191ac9
Densitie <arkir7294@ugcloud.ca>
2019-10-01 10:35:39 -0700

    Summative Project

--- src/MultipleChoiceQuestion.java
+++ src/MultipleChoiceQuestion.java
@@ -1,12 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Arun K
+ *Septembre 28 2019
  */
 
 /**
  *
- * @author ajayk
+ * @author arkir7294
  */
 public class MultipleChoiceQuestion extends javax.swing.JFrame {
      int incorrect = 0;
